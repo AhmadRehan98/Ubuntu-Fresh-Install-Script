48c98369a98721cd3eac9b1c3762a5f00edd19d0
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-08-26 21:11:23 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ install_or_skip() {
   fi
 }
 sudo apt update
-sudo apt-get install curl
+sudo apt-get install curl git 
 
 install_or_skip "Set" "touchpad scrolling direction up=up and down=down" "gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false"
 install_or_skip "Set" "click on start bar minimizes or previews" "gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-previews'"
@@ -28,3 +28,4 @@ install_or_skip "Install" "microsoft teams" "sudo snap install --classic teams-f
 install_or_skip "Install" "postman" "sudo snap install --classic postman"
 install_or_skip "Install" "microsoft edge" "curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg; sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/; sudo sh -c 'echo \"deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main\" > /etc/apt/sources.list.d/microsoft-edge-dev.list'; sudo rm microsoft.gpg; sudo apt update; sudo apt-get install microsoft-edge-stable"
 install_or_skip "Install" "gnome tweaks" "sudo add-apt-repository universe; sudo apt-get install gnome-tweaks"
+install_or_skip "Install" "github desktop" "wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null; sudo sh -c 'echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main\" > /etc/apt/sources.list.d/shiftkey-packages.list'; sudo apt update; sudo apt-get install github-desktop"
